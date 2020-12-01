0c3694aac4c873dc6b28bb0fbe3774b0ed57f834
youmingxu <71304475+youmingxu@users.noreply.github.com>
2020-12-01 11:46:50 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://youmingxu.github.io/frog-10932613/model.json';
 
 
 function preload() {
